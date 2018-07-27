d54d4d6b4057bd0a10f7d9c16a80ae2d413587d5
ankrajagent <ankit@agentdesks.com>
2018-07-28 02:14:21 +0530

    fix

--- smartVizx/directives.js
+++ smartVizx/directives.js
@@ -1,1 +1,34 @@
 
+app.directive('numericalSlider', [function () {
+    return {
+      restrict: 'EA',
+      scope: {
+        'min' : '<',
+        'max' : '<',
+        'value' : '<'
+      },
+      templateUrl: 'smartVizx/numericalSlider.html',
+      link: function (scope, element, attrs) {
+
+          var sliderWidth = $('.sliderWrapper').width();
+          var sliderLength = parseInt(scope.max) - parseInt(scope.min);
+          $('.sliderParentWrapper').css('width', sliderWidth + 70 + 'px');
+          var startpixel = $('.sliderParentWrapper').offset();
+          var initialPoint = ((scope.value - parseInt(scope.min)) * sliderWidth / sliderLength ) + startpixel.left;
+          $( "#bubble" ).offset({ left: initialPoint });
+          $('.colored-line').css('width', initialPoint - startpixel.left + 'px');
+
+          $("#bubble").draggable({
+            axis: "x",
+            containment: ".sliderParentWrapper",
+            drag: function( event, ui ) {
+              var startpixel = $('.sliderParentWrapper').offset();
+              var point = $('#bubble').offset().left;
+              $('.colored-line').css('width', point - startpixel.left + 'px');
+              scope.value = parseInt((point - startpixel.left + 1) * sliderLength / sliderWidth) + parseInt(scope.min);
+              scope.$digest();
+            }
+          });
+      }
+    };
+}]);
--- smartVizx/slider/slider.controller.js
+++ smartVizx/slider/slider.controller.js
@@ -3,34 +3,5 @@
 app.controller('sliderCtrl', ['$scope',
   function($scope) {
 
-        $scope.slider = {
-          value: 63,
-          options: {
-            floor: 25,
-            ceil: 120,
-            step: 1,
-            showSelectionBar: true
-          }
-        }
-
-        $scope.firstDotDistance = 68;
-        var sliderWidth = $('.sliderWrapper').width();
-        var sliderLength = parseInt($('.right-end').text()) - parseInt($('.left-end').text());
-        $('.sliderParentWrapper').css('width', sliderWidth + 70 + 'px');
-        var startpixel = $('.sliderParentWrapper').offset();
-        var initialPoint = (($scope.firstDotDistance - parseInt($('.left-end').text()) + 1) * sliderWidth / (100 * sliderLength/100) ) + startpixel.left;
-        $( "#bubble" ).offset({ left: initialPoint });
-        $('.colored-line').css('width', initialPoint - startpixel.left + 'px');
-
-        $("#bubble").draggable({
-          axis: "x",
-          containment: ".sliderParentWrapper",
-          drag: function( event, ui ) {
-            var point = $('#bubble').offset().left;
-            $('.colored-line').css('width', point - startpixel.left + 'px');
-            $scope.firstDotDistance = parseInt((point - startpixel.left + 1) * (100 * sliderLength/100) / sliderWidth) + parseInt($('.left-end').text());
-            $scope.$digest();
-          }
-        });
 
 }]);
